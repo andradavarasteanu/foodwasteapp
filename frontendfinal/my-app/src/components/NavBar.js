f786816131a3fc2c90e08d5e016fb376e14377bf
elena <elenavisan2001@yahoo.com>
2023-01-17 00:28:34 +0200

    first commit

---
+++ frontendfinal/my-app/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import './navbar.css';
+import { Link, useMatch,useResolvedPath} from 'react-router-dom';
+import { useImperativeHandle } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function NavBar(){
+    const {id}=useParams();
+    return <nav className="nav">
+    <Link to="/1" className="site-title">Food Waste App</Link>
+    <ul>
+    <CustomLink to={`/1/Friends`}>Friends</CustomLink>
+    <CustomLink to={`/1/MyFridge`}>My fridge</CustomLink>
+    </ul>
+    </nav>
+}
+
+function CustomLink({to,children,...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive=useMatch({path: resolvedPath.pathname, end: true})
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to}{...props}>{children}</Link>
+        </li>
+    )
+}
